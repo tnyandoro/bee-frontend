e9c8f0d71def1f6892984ec00262ecfadd0a45a2
Tendai <tendai1@hotmail.com>
2025-07-06 15:36:22 +0200

    Add the adminDashboard shaking cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
       backgroundImage: {
         "main-bg": "url('/src/assets/main_bg.png')",
       },
+      animation: {
+        shake: "shake 0.3s ease-in-out",
+      },
+      keyframes: {
+        shake: {
+          "0%, 100%": { transform: "translateX(0)" },
+          "25%": { transform: "translateX(-5px)" },
+          "75%": { transform: "translateX(5px)" },
+        },
+      },
     },
   },
   plugins: [],
